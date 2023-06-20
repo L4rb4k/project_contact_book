ff2a9a731dfb460a66d7aa254831a2c436b0bfdb
Seu nome <seu_email@gmail.com>
2023-06-20 10:14:59 +0100

    Dynamic form creation field and non_field errors

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,54 @@
+from typing import Any, Dict
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(data=request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    context = {
+        'form': ContactForm()
     }
 
     return render(
