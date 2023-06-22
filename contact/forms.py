958f4ec748655ae19449a24cc2787f8b26abc9a8
jmbhcabral <larbak.cstrike@gmail.com>
2023-06-22 08:33:48 +0100

    Fields validation first_name/last_name/email

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from . import models
 
 
@@ -52,4 +53,35 @@ class ContactForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+        # error_messages={
+        #     'required': 'Tem de preencher este campo.',
+        #     'min_length': 'O sobrenome tem de ter no minimo 3 caracteres.'
+        # }
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este email', code='invalid'),
+            )
+        return email
