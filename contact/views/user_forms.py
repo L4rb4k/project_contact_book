95cfa3e5dc1eb93b349b0ebfa0b11dba79e567de
jmbhcabral <larbak.cstrike@gmail.com>
2023-06-23 17:26:42 +0100

    User update form creation

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,26 +1,19 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib import auth
+from django.shortcuts import redirect, render
+
 from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
     form = RegisterForm()
-
-    # messages.info(request, 'Um texto qualquer.')
-    # messages.success(request, 'Um texto qualquer.')
-    # messages.warning(request, 'Um texto qualquer.')
-    # messages.error(request, 'Um texto qualquer.')
-
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-
         if form.is_valid():
             form.save()
-            messages.success(request, 'Usuário Registado.')
+            messages.success(request, 'Usuário registrado')
             return redirect('contact:login')
-
     return render(
         request,
         'contact/register.html',
@@ -30,19 +23,44 @@ def register(request):
     )
 
 
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:user_update')
+
+
 def login_view(request):
     form = AuthenticationForm(request)
 
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
-
         if form.is_valid():
             user = form.get_user()
             auth.login(request, user)
             messages.success(request, 'Logado com sucesso!')
             return redirect('contact:index')
-        messages.error(request, 'Login inválido.')
-
+        messages.error(request, 'Login inválido')
     return render(
         request,
         'contact/login.html',
